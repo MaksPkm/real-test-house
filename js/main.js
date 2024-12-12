c72bacfddce87a09e22ba5b6cacaca0a5dbbb5f1
Maksym <maksymkoshelap@gmail.com>
2024-12-12 16:06:42 +0100

    last add

--- js/main.js
+++ js/main.js
@@ -27,3 +27,49 @@ phoneInputs.forEach((input) => {
     })
 });
 
+
+
+//  Map
+
+ymaps.ready(init);
+function init(){
+	
+	const map = new ymaps.Map('map', {
+		
+		center: [46.4774700, 30.7326200],
+		
+		zoom: 9,
+	});
+
+
+    const myPlacemark = new ymaps.Placemark(
+		[46.4774700, 30.7326200],
+		{
+			balloonContent: `
+				<div class="balloon">
+					<div class="balloon__address">Наб. реки Фонтанки 10-15</div>
+					<div class="balloon__contacts">
+						<a href="tel:+78121234567">+8 (812) 123-45-67</a>
+					</div>
+				</div>
+			`,
+		},
+		{
+			iconLayout: 'default#image',
+			iconImageHref: './img/map/location-pin.svg',
+			iconImageSize: [40, 40],
+			iconImageOffset: [-20, -40],
+		}
+	);
+
+
+    map.controls.remove('geolocationControl'); 
+	map.controls.remove('searchControl'); 
+	map.controls.remove('trafficControl'); 
+	map.controls.remove('typeSelector'); 
+	map.controls.remove('rulerControl'); 
+	map.behaviors.disable(['scrollZoom']);
+
+	map.geoObjects.add(myPlacemark);
+    myPlacemark.balloon.open();
+}
