3999064026c5517a8389adf766bd875c1c0acff3
Maksym <maksymkoshelap@gmail.com>
2024-12-12 11:40:32 +0100

    almost done update

--- js/main.js
+++ js/main.js
@@ -12,3 +12,18 @@ navBtn.onclick = function () {
 
     document.body.classList.toggle('no-scroll');
 }
+
+// Phone Mask
+
+mask('[data-tel-input]');
+
+const phoneInputs = document.querySelectorAll('[data-tel-input]');
+phoneInputs.forEach((input) => {
+    input.addEventListener('input', () => {
+        if (input.value == '+') input.value = '';
+    })
+    input.addEventListener('blur', () => {
+        if (input.value == '+') input.value = ''; 
+    })
+});
+
